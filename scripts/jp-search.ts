3da199dbbdc8a33eb2d24f2f11075e93e464cc21
Pacharapol Withayasakpunt <polv@polv.cc>
2023-12-10 21:57:09 +0700

    update

--- scripts/jp-search.ts
+++ scripts/jp-search.ts
@@ -1,15 +1,16 @@
 import 'dotenv/config';
 
+import fg from 'fast-glob';
+import { readFileSync } from 'fs';
+
 import { Tatoeba } from '@/tatoeba';
-import createConnectionPool, { sql } from '@databases/pg';
+import createConnectionPool, { ConnectionPool, sql } from '@databases/pg';
 
-async function main() {
-  const db = createConnectionPool({
-    connectionString: process.env['POSTGRES_URI'],
-    bigIntMode: 'number',
-  });
-  // await db.query(sql.file('scripts/pg/sentence.sql'));
+async function create(db: ConnectionPool) {
+  await db.query(sql.file('scripts/pg/sentence.sql'));
+}
 
+async function addTatoeba(db: ConnectionPool) {
   const t = new Tatoeba();
 
   await db.tx(async (db) => {
@@ -28,6 +29,40 @@ async function main() {
   });
 
   t.db.close();
+}
+
+async function addImKit(db: ConnectionPool) {
+  const cwd = 'C:\\Users\\Hp\\Projects\\immersion-kit-api\\resources';
+  for await (const f of fg.stream('**/data.ndjson', { cwd, absolute: true })) {
+    const rows = readFileSync(f, 'utf-8')
+      .trimEnd()
+      .split('\n')
+      .map((r, i) => {
+        const d = JSON.parse(r);
+        return { ...d, i };
+      });
+
+    while (rows.length) {
+      await db.query(
+        sql`INSERT INTO sentence ("text", "source") VALUES ${sql.join(
+          rows.splice(0, 10000).map((d) => {
+            return sql`(${d.sentence}, ${`${d.id}(${d.i + 1})`})`;
+          }),
+          ',',
+        )} ON CONFLICT ("source") DO NOTHING`,
+      );
+    }
+  }
+}
+
+async function main() {
+  const db = createConnectionPool({
+    connectionString: process.env['POSTGRES_URI'],
+    bigIntMode: 'number',
+  });
+
+  await addImKit(db);
+
   await db.dispose();
 }
 
--- scripts/pg/sentence.sql
+++ scripts/pg/sentence.sql
@@ -1,10 +1,9 @@
-DROP TABLE links;
-DROP TABLE sentence;
-
 CREATE TABLE IF NOT EXISTS sentence (
   "text"      TEXT NOT NULL,
-  tatoeba_id  INTEGER
+  tatoeba_id  INTEGER,
+  source      TEXT
 );
 
 CREATE INDEX IF NOT EXISTS idx_sentence_text ON sentence USING pgroonga("text");
-CREATE INDEX IF NOT EXISTS idx_sentence_tatoeba_id ON sentence (tatoeba_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_sentence_tatoeba_id ON sentence (tatoeba_id);
+CREATE UNIQUE INDEX IF NOT EXISTS idx_sentence_source ON sentence (source);
